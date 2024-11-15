5e337c2e0b39d428dfe977f5ce6b4d4cc82d9045
Sam Rynearson <samryno@gmail.com>
2024-11-14 20:26:42 -0700

    Resolve merge conflicts
    Update Author Details

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     description="A simple text styling package for console output",
     author="Sam Rynearson",
-    author_email="samryno@gmail.com",
+    author_email="s.c.rynearson@gmail.com",
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3.6',
